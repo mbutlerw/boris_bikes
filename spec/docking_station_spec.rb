84e5da323718f721fb7e409797f445465faffce0
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-26 09:09:44 +0100

    fixed the test for the working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,10 @@ require 'docking_station'
 describe DockingStation do
 	
 
-	# it "returns a working bike" do
-	# 	bike = subject.release_bike
-	# 	expect(bike).to be_working
-	# end
+	it "returns a working bike" do
+		bike = subject.release_bike
+		expect(bike).to be_working
+	end
 
 	it "responds to dock" do
 		expect(subject).to respond_to(:dock).with(1).argument
