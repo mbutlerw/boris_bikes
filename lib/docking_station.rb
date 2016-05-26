4849d293b558b3e882eac7df0e9eb81f77d24e41
danielschambers <daniel.s.chambers@gmail.com>
2016-05-26 11:36:20 +0100

    bleh

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,17 +3,19 @@ require_relative 'bike'
 class DockingStation
 
 
-attr_reader :bike
+attr_reader :bikes
+
+
 
 # in this case, @bike is actually 'nil' - which EVALUATES to 'false' (falsey)
 def release_bike
-	fail 'No bikes available' unless @bike
-	@bike
+	fail 'No bikes available' unless @bikes
+	@bikes
 end
 
 def dock(bike)
-	fail 'Exceeded capacity' if @bike
-	@bike = bike
+	fail 'Exceeded capacity' if @bikes
+	@bikes << bike
 
 end
 
